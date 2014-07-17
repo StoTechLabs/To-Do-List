14f3b4f3731bc307d38487b222b40e79aa3cc299
joshua Storey & Carolyn Huynh <student@epicodus.com>
2014-07-17 15:01:27 -0700

    2nd commit, jQuery works

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,50 @@
 $(document).ready(function() {
-  $("form#task").submit(function(event) {
-    var inputtedTask = $("#task-item").val();
-    var tasks = {
-      description: inputtedTask
-    };
-    $("#task-list").append("<li><span class='task-item'>" + tasks.description + " </span></li>");
+  var currentList;
+  $("form#create-list").submit(function(event) {
+    var inputtedList = $("input#list-item").val().toUpperCase();
 
-    $(".task-item").last().click(function() {
-      $(this).addClass("task-change");
+    var newList = { list: inputtedList, tasks: [] };
+    $('.list-items').append("<li><span class='list-name'>" + newList.list + "</span></li>");
+
+
+    $('.list-name').last().click(function() {
+      $('#header').text(newList.list);
+      currentList = newList;
+      $(".task-list").empty();
+      currentList.tasks.forEach(function(task) {
+        $('.task-list').append("<li>" + task.task + "</li>");
+      });
+
     });
 
 
+    event.preventDefault();
+    $("input#list-item").val("");
+  });
+
+
+  $("form#create-task").submit(function(event) {
+    var inputtedTask = $("input#task-item").val().toUpperCase();
+
+    var newTaskItem = { task: inputtedTask };
+    $('.task-list').append("<li>" + newTaskItem.task + "</li>");
+    currentList.tasks.push(newTaskItem);
+
 
     event.preventDefault();
+    $("input#task-item").val("");
   });
 
 });
+
+
+
+    // var inputtedTask = $("#task-item").val();
+    // var tasks = {
+    //   description: inputtedTask
+    // };
+    // $("#task-list").append("<li><span class='task-item'>" + tasks.description + " </span></li>");
+
+    // $(".task-item").last().click(function() {
+    //   $(this).addClass("task-change");
+    // });
--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-This is the first version of my To Do List site.
+This is a  version of a To Do List. The user can create as many lists as they want and create individual tasks within those lists.
